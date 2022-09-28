54eb2fd03fdcc51c1458d6404c583ab1ce8198c9
ktrzaskoma <kttrzaskoma@gmail.com>
2022-09-28 22:15:30 +0200

    init

---
+++ src/main/java/springspotifyapi/SpringSpotifyApiApplication.java
@@ -0,0 +1,13 @@
+package springspotifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpotifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpotifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springspotifyapi/SpringSpotifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package springspotifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpotifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
